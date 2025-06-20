80ea8ed2340eed001ea789b5378bb206eef15033
Gabriel Reis Klein <gabrielreisklein@gmail.com>
2025-06-19 22:50:49 -0300

    Add files via upload

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,9 +5,16 @@ import { MessageService } from 'primeng/api';
 import Aura from '@primeng/themes/aura';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideAnimationsAsync(),
     provideRouter(routes),
